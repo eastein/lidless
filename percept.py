df0e18593868c9c4b7d501961a775feb151dfe79
Eric Stein <toba@des.truct.org>
2011-08-02 23:59:36 -0500

    show a histogram of inter-frame edge-pixel counts, binned in squares

--- percept.py
+++ percept.py
@@ -1,6 +1,9 @@
 import zmstream
 import cv
+import math
+import pprint
 import os
+import sys
 import tempfile
 
 class Percept :
@@ -26,14 +29,60 @@ class Percept :
 		cv.Canny(med, canny, 75, 112, 3)
 		return canny
 
-if __name__ == '__main__' :
-	import sys
+	def dict_diff(self, d1, d2) :
+		key_merge = set(d1.keys()).intersection(d2.keys())
+		result = {}
+		for k in key_merge :
+			result[k] = abs(d1[k] - d2[k])
+		return result
+	
+	def bin_edgecount(self, img, bins=24) :
+		sz = cv.GetSize(img)
+		twi = sz[0]
+		thi = sz[1]
+		tw = float(twi)
+		th = float(thi)
+		ta = tw * th
+		r = tw/th
+		a = ta / bins
+		h = int(math.pow(a / r, 0.5))
+		w = int(a / h)
 
+		# bin maximums, integer rounded
+		wbm = twi / w
+		hbm = thi / h
+		whitecounts = {}
+		for wb in range(wbm) :
+			for hb in range (hbm) :
+				whitecounts[wb, hb] = 0
+
+		for wo in range(wbm * w) :
+			for ho in range(hbm * h) :
+				# integer division for the counts, find bin numbers for current pixel
+				b_w = wo / w
+				b_h = ho / h
+				v = img[ho, wo]
+				if v > 0 :
+					whitecounts[b_w, b_h] += 1
+
+		return whitecounts
+
+if __name__ == '__main__' :
 	p = Percept()
 	url = sys.argv[1]
 	streamer = zmstream.ZMStreamer(1, url)
+	edge_bin = {}
 	for i in streamer.generate() :
 		img = p.image_jpegstr(i)
 		filtered = p.filter_edges(img)
+
 		cv.SaveImage('edges.png', filtered)
-		sys.exit(1)
+
+		new_bin = p.bin_edgecount(filtered)
+		diff = p.dict_diff(new_bin, edge_bin)
+		edge_bin = new_bin
+
+		for k in diff :
+			diff[k] = '=' * (diff[k] / 5)
+
+		pprint.pprint(diff)
