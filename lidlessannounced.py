083f07f6ee33668e0813a7646a7aeb04880719db
Eric Stein <toba@des.truct.org>
2012-03-31 01:23:05 -0500

    #41; interface with announced.

---
+++ lidlessannounced.py
@@ -0,0 +1,24 @@
+import Queue
+import time
+import threading
+import zmqsub
+
+class LidlessAnnouncer(threading.Thread) :
+	def __init__(self, alerts, zmq_url) :
+		self.alerts = alerts
+		self.zmqpub = zmqsub.JSONZMQConnectPub(zmq_url)
+		self.ok = True
+		threading.Thread.__init__(self)
+	
+	def stop(self) :
+		self.ok = False
+
+	def run(self) :
+		while self.ok :
+			time.sleep(1)
+			for alert in self.alerts :
+				try :
+					alert = alert.q.get(timeout=0)
+					self.zmqpub.send({'text' : alert, 'pitch' : -180})
+				except Queue.Empty :
+					pass
